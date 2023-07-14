92e69ab5c203a213b982c4d0d71ebebbda17bfbd
Christian Verdugo <crischandel@gmail.com>
2023-07-14 00:03:41 -0400

    Mejoras y solucion re errores

--- EnlazarDirectorios.py
+++ EnlazarDirectorios.py
@@ -1,4 +1,5 @@
-import os  # Libreria para ejecutar comando de consola
+import os
+from os.path import isdir  # Libreria para ejecutar comando de consola
 
 # Proximas mejoras si existe el directorio .config/<programa>
 # cambiarle el nombre y seguir con lo otro
@@ -8,28 +9,27 @@ def EnlazarDirectorio(programa):
     # ademas sumando el nombre del programa a instalar
     ruta_config = os.path.expanduser("~") + f"/.config/{programa}"
     
-    # Obtenemos la direccion del archivo .py para obtener la
-    # direccion de la carpeta .config del repo para enlazarlo
-    ruta_Enlace = os.path.dirname(
-        os.path.abspath(__file__)) + f"/.config/{programa}"
+    if (os.path.isdir(ruta_config)):
+        pass
+    else:
+        # Obtenemos la direccion del archivo .py para obtener la
+        # direccion de la carpeta .config del repo para enlazarlo
+        ruta_Enlace = os.path.dirname(
+            os.path.abspath(__file__)) + f"/.config/{programa}"
    
-    # Comando para crear en enlace simbolico
-    comandoEnlazar = f"ln -s {ruta_Enlace} {ruta_config}"
-    resultado = os.system(comandoEnlazar) # Ejecuta comando
+        # Comando para crear en enlace simbolico
+        comandoEnlazar = f"ln -s {ruta_Enlace} {ruta_config}"
+        resultado = os.system(comandoEnlazar) # Ejecuta comando
 
 def DesenlazarDirectorio(programa):
-    # Obtenemos ruta de la configuracion a desenlazar
+
+        # Obtenemos ruta de la configuracion a desenlazar
     ruta_config = os.path.expanduser("~") + f"/.config/{programa}"
-    comando = f"unlink {ruta_config}"
-    resultado = os.system(comando)
+    if os.path.isdir(ruta_config):
+        comando = f"unlink {ruta_config}"
+        resultado = os.system(comando)
 
-programas = ["qtile", "dunst", "picom","nvim" "alacritty"]
-
+programas = ["qtile", "dunst", "picom","nvim", "alacritty"]
 
 for i in programas:
     EnlazarDirectorio(i)
-
-
-
-#print(EnlazarDirectorio("dunst"))
-#print(DesenlazarDirectorio("dunst"))
